e04b619043bf41fc894d8f4eccf9d92572767f77
karolNiedziela <imniedziel@gmail.com>
2022-06-05 21:26:49 +0200

    Remove additional passwordmatch function

--- angularclient/src/app/shared/validators/password.validator.ts
+++ angularclient/src/app/shared/validators/password.validator.ts
@@ -15,27 +15,6 @@ export function passwordStrengthValidator(
       };
 }
 
-export function passwordMatch(
-  controlName: string,
-  matchingControlName: string
-) {
-  return (formGroup: FormGroup) => {
-    const control = formGroup.controls[controlName];
-    const matchingControl = formGroup.controls[matchingControlName];
-    if (
-      matchingControl.errors &&
-      !matchingControl.errors['confirmedValidator']
-    ) {
-      return;
-    }
-    if (control.value !== matchingControl.value) {
-      matchingControl.setErrors({ confirmedValidator: true });
-    } else {
-      matchingControl.setErrors(null);
-    }
-  };
-}
-
 export function matchPassword(control: AbstractControl) {
   const password = control.get('password')?.value;
 
