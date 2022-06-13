9a828d1a852c1db4f239c986fcf84209735b953e
karolNiedziela <imniedziel@gmail.com>
2022-06-13 18:49:57 +0200

    Repair excluding personId from swagger schema, add example for endpoint

--- API/src/Shared/SharedHome.Shared/DependencyInjection.cs
+++ API/src/Shared/SharedHome.Shared/DependencyInjection.cs
@@ -1,12 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
 using SharedHome.Shared.Abstractions.Time;
 using SharedHome.Shared.Abstractions.User;
 using SharedHome.Shared.Email;
 using SharedHome.Shared.Exceptions;
-using SharedHome.Shared.Filters;
 using SharedHome.Shared.Time;
 using SharedHome.Shared.User;
 
@@ -14,9 +12,6 @@ namespace SharedHome.Shared
 {
     public static class DependencyInjection
     {
-        private const string ApiTitle = "HomeShared API";
-        private const string ApiVersion = "v1";
-
         public static IServiceCollection AddShared(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddEmail(configuration);
@@ -27,19 +22,7 @@ namespace SharedHome.Shared
             services.AddHttpContextAccessor();
             services.AddScoped<ICurrentUser, CurrentUser>();
 
-            services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen(swagger =>
-            {
-                swagger.EnableAnnotations();
-                swagger.CustomSchemaIds(x => x.FullName);
-                swagger.SwaggerDoc(ApiVersion, new OpenApiInfo
-                {
-                    Title = ApiTitle,
-                    Version = ApiVersion
-                });
-
-                swagger.OperationFilter<SwaggerExcludeFilter>();
-            });
+            services.AddEndpointsApiExplorer();      
 
             services.Configure<GeneralSettings>(configuration.GetSection(GeneralSettings.SectionName));
 
---
+++ API/src/SharedHome.Api/Swagger/Examples/Requests/ShoppingLists/AddShoppingListExample.cs
@@ -0,0 +1,16 @@
+﻿using SharedHome.Application.ShoppingLists.Commands;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace SharedHome.Api.Swagger.Examples.Requests.ShoppingLists
+{
+    public class AddShoppingListExample : IExamplesProvider<AddShoppingList>
+    {
+        public AddShoppingList GetExamples()
+        {
+            return new AddShoppingList
+            {
+                Name = "Lidl"
+            };
+        }
+    }
+}
