c7c096560028f189b9c4529c19537c2a11fd73ae
karolNiedziela <imniedziel@gmail.com>
2022-08-08 18:13:53 +0200

    Move configurations to own class

--- API/src/SharedHome.Api/DepedencyInjection.cs
+++ API/src/SharedHome.Api/DepedencyInjection.cs
@@ -1,20 +1,61 @@
-﻿using Microsoft.OpenApi.Models;
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.OpenApi.Models;
 using SharedHome.Api.Swagger.Filters;
+using SharedHome.Shared.Constants;
 using Swashbuckle.AspNetCore.Filters;
+using System.Globalization;
 using System.Reflection;
+using System.Text.Json.Serialization;
 
-namespace SharedHome.Api.Extensions
+namespace SharedHome.Api
 {
-    public static class ServiceCollectionExtensions
+    public static class DepedencyInjection
     {
         private const string ApiTitle = "HomeShared API";
         private const string ApiVersion = "v1";
 
         public static IServiceCollection AddApi(this IServiceCollection services)
         {
+            services.AddControllers()
+               .AddJsonOptions(options =>
+               {
+                   options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+               })
+               .AddDataAnnotationsLocalization(options =>
+               {
+                   options.DataAnnotationLocalizerProvider = (type, factory) =>
+                   {
+                       return factory.Create(Resources.DataAnnotationMessage, Assembly.GetEntryAssembly()!.GetName().Name!);
+                   };
+               });
+
+            services.AddMvc(options =>
+            {
+                options.SuppressAsyncSuffixInActionNames = false;
+            });
+
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new List<CultureInfo>()
+                {
+                    new CultureInfo("en-US"),
+                    new CultureInfo("pl-PL")
+                };
+
+                options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
+
             AddCors(services);
 
             AddSwagger(services);
+
+            services.AddRouting(options =>
+            {
+                options.LowercaseUrls = true;
+            });
 
             return services;
         }
--- API/src/SharedHome.Api/Program.cs
+++ API/src/SharedHome.Api/Program.cs
@@ -1,16 +1,11 @@
-using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Options;
 using Serilog;
+using SharedHome.Api;
 using SharedHome.Api.Constants;
-using SharedHome.Api.Extensions;
 using SharedHome.Application;
 using SharedHome.Identity;
 using SharedHome.Infrastructure;
 using SharedHome.Shared;
-using SharedHome.Shared.Constants;
-using System.Globalization;
-using System.Reflection;
-using System.Text.Json.Serialization;
 
 try
 {
@@ -27,51 +22,11 @@ try
     builder.Host.UseSerilog((context, serviceProvider) => serviceProvider
           .ReadFrom.Configuration(configuration));
 
-    // Add services to the container.    
-    builder.Services.AddControllers()
-        .AddJsonOptions(options =>
-        {
-            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-        })
-        .AddDataAnnotationsLocalization(options =>
-        {
-            options.DataAnnotationLocalizerProvider = (type, factory) =>
-            {
-                return factory.Create(Resources.DataAnnotationMessage, Assembly.GetEntryAssembly()!.GetName().Name!);
-            };
-        });
-
-    builder.Services.AddMvc(options =>
-    {
-        options.SuppressAsyncSuffixInActionNames = false;
-    });
-
-    builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
-    builder.Services.Configure<RequestLocalizationOptions>(options =>
-    {
-        var supportedCultures = new List<CultureInfo>()
-        {
-            new CultureInfo("en-US"),
-            new CultureInfo("pl-PL")
-        };
-
-        options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
-        options.SupportedCultures = supportedCultures;
-        options.SupportedUICultures = supportedCultures;
-    });
-
     builder.Services.AddSharedHomeIdentity(builder.Configuration);
     builder.Services.AddApplication();
     builder.Services.AddInfrastructure(builder.Configuration);
     builder.Services.AddShared(builder.Configuration);
-
     builder.Services.AddApi();
-
-
-    builder.Services.AddRouting(options =>
-    {
-        options.LowercaseUrls = true;
-    });
 
     var app = builder.Build();
 
