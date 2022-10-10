68bea00031abc1b71cf2f1c26117c8aa470d7752
karolNiedziela <imniedziel@gmail.com>
2022-10-10 17:40:49 +0200

    Fix problem with Accept-Language header

--- angularclient/src/app/core/interceptors/language.interceptor.ts
+++ angularclient/src/app/core/interceptors/language.interceptor.ts
@@ -15,8 +15,24 @@ export class LanguageInterceptor implements HttpInterceptor {
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
+    let language: string = '';
+
+    switch (navigator.language) {
+      case 'pl':
+        language = 'pl-PL';
+        break;
+
+      case 'en':
+        language = 'en-US';
+        break;
+
+      default:
+        language = 'en-US';
+        break;
+    }
+
     request = request.clone({
-      headers: request.headers.set('Accept-Language', navigator.language),
+      headers: request.headers.set('Accept-Language', language),
     });
 
     return next.handle(request);
