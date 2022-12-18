fab2e368a4a72251941d78bcd67c2de15127ef4f
karolNiedziela <imniedziel@gmail.com>
2022-12-18 18:15:13 +0100

    SH-46: Add endpoint for user information, move changing language and theme to navbar, fix problem with clearing error messages

---
+++ angularclient/src/app/modules/identity/models/user-information.ts
@@ -0,0 +1,6 @@
+export interface UserInformation {
+  firstName: string;
+  lastName: string;
+  email: string;
+  profileImageUrl?: string;
+}
--- angularclient/src/app/shared/components/other/theme-switcher/theme-switcher.component.ts
+++ angularclient/src/app/shared/components/other/theme-switcher/theme-switcher.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { faMoon } from '@fortawesome/free-regular-svg-icons';
 import { ThemeService } from 'app/core/services/theme/theme.service';
-
 @Component({
   selector: 'app-theme-switcher',
   templateUrl: './theme-switcher.component.html',
   styleUrls: ['./theme-switcher.component.scss'],
 })
 export class ThemeSwitcherComponent implements OnInit {
+  themeIcon = faMoon;
+
   constructor(private themeService: ThemeService) {}
 
   ngOnInit(): void {}
